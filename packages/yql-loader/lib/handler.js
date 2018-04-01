e861abddea795b7fc6d6db937cbcf9ad7c3c3089
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2018-04-01 23:53:13 +0800

    yql-loader@0.2.2

--- packages/yql-loader/lib/handler.js
+++ packages/yql-loader/lib/handler.js
@@ -1,6 +1,7 @@
 module.exports = function (request, url, operation, variables, options, metadata) {
   options = options || {};
   metadata = metadata || options.metadata;
+  request = request.default || request;
   var data = {
     operation: operation,
     variables: JSON.stringify(variables),
