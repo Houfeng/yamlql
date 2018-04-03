3f04329e7d192a4328fceecc27e318c9db999193
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2018-04-03 12:56:55 +0800

    0.1.8

--- src/builtIn.ts
+++ src/builtIn.ts
@@ -3,95 +3,95 @@ const { isNull, iArray, isString, formatDate } = require('ntils');
 export default class BuiltIn {
 
   // map 直接返回就行，YamlQL 本身即可能 map
-  public _map(arg: any) {
+  public __map(arg: any) {
     return arg;
   }
 
-  public _slice(list: Array<any>, start: number, end: number) {
+  public __slice(list: Array<any>, start: number, end: number) {
     if (!list) return list;
     if (!iArray(list)) return [];
     return list.slice(start, end);
   }
 
-  public _limit(list: Array<any>, skip: number, limit: number) {
+  public __limit(list: Array<any>, skip: number, limit: number) {
     if (!list) return list;
     if (!iArray(list)) return [];
     return list.slice(skip, skip + limit);
   }
 
-  public _reverse(list: Array<any>) {
+  public __reverse(list: Array<any>) {
     if (!list) return list;
     if (!iArray(list)) return [];
     return list.reverse();
   }
 
-  public _concat(list: Array<any>, ...args: Array<any>) {
+  public __concat(list: Array<any>, ...args: Array<any>) {
     if (!list) return list;
     if (!iArray(list)) return [];
     return list.concat(list, ...args);
   }
 
-  public _keys(obj: any) {
+  public __keys(obj: any) {
     return Object.keys(obj);
   }
 
-  public _values(obj: any) {
+  public __values(obj: any) {
     return Object.values(obj);
   }
 
-  public _find(list: Array<any>, name: string, value: any) {
+  public __find(list: Array<any>, name: string, value: any) {
     if (!list || !iArray(list)) return;
     return list.find(item => item === name) ||
       list.find(item => item[name] === value);
   }
 
-  public _filter(list: Array<any>, name: string, value: any) {
+  public __filter(list: Array<any>, name: string, value: any) {
     if (!list || !iArray(list)) return [];
     return isNull(value) ? list.filter(item => item === name)
       : list.filter(item => item[name] === value);
   }
 
-  public _join(list: Array<string>, split: string) {
+  public __join(list: Array<string>, split: string) {
     if (!list) return '';
     if (!iArray(list)) return list;
     return list.join(split);
   }
 
-  public _split(str: string, split: string) {
+  public __split(str: string, split: string) {
     if (!str) return str;
     if (!isString(str)) return [str];
     return str.split(split);
   }
 
-  public _substr(str: string, start: number, length: number) {
+  public __substr(str: string, start: number, length: number) {
     if (!str) return str;
     return str.substr(start, length);
   }
 
-  public _substring(str: string, start: number, end: number) {
+  public __substring(str: string, start: number, end: number) {
     if (!str) return str;
     return str.substring(start, end);
   }
 
-  public _number(str: string) {
+  public __number(str: string) {
     return Number(str);
   }
 
-  public _string(obj: any) {
+  public __string(obj: any) {
     if (!obj) return String(obj);
     return obj.toString();
   }
 
-  public _boolean(obj: any) {
+  public __boolean(obj: any) {
     return Boolean(obj);
   }
 
-  public _date(obj: any, format?: string) {
+  public __date(obj: any, format?: string) {
     const date = new Date(obj);
     return format ? formatDate(date, format) : date;
   }
 
-  public _parse(str: string) {
+  public __parse(str: string) {
     try {
       return JSON.parse(str);
     } catch{
@@ -99,25 +99,25 @@ export default class BuiltIn {
     }
   }
 
-  public _stringify(obj: any) {
+  public __stringify(obj: any) {
     return JSON.stringify(obj);
   }
 
-  public _merge(...args: Array<any>) {
+  public __merge(...args: Array<any>) {
     return Object.assign({}, ...args);
   }
 
-  public _object(...args: Array<any>) {
+  public __object(...args: Array<any>) {
     return Object.assign({}, ...args);
   }
 
-  public _array(array: any) {
+  public __array(array: any) {
     if (!array) return array;
     if (!('length' in array)) array.length = Object.keys(array).length;
     return [].slice.call(array);
   }
 
-  public _value(value: any) {
+  public __value(value: any) {
     return value;
   }
 
