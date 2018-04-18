2f1c979f6f540ab155952dc7dfc557bc6820de28
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2018-04-18 20:09:28 +0800

    yql-loader 默认 withCredentials 设为 true

--- packages/yql-loader/lib/request.js
+++ packages/yql-loader/lib/request.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 
+axios.defaults.withCredentials = true;
+
 module.exports = function request(url, data, options) {
   return axios.post(url, data, options).then(function (rs) {
     return rs.data;
