3bdefa5c8e8c520025135222cdfd7b4169eaa03f
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2018-05-31 13:09:57 +0800

    重构建变量名

--- packages/yamlql-loader/lib/request.js
+++ packages/yamlql-loader/lib/request.js
@@ -3,8 +3,8 @@ var axios = require('axios');
 axios.defaults.withCredentials = true;
 
 //默认的 request 模块
-module.exports = function request(url, data, options) {
-  return axios.post(url, data, options).then(function (rs) {
+module.exports = function request(endpoint, data, options) {
+  return axios.post(endpoint, data, options).then(function (rs) {
     return rs.data;
   });
 };
