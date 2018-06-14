41fd10725381231b8b675e16fad5074680ae9abd
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2018-06-14 22:52:07 +0800

    yamlql@0.1.16

--- src/Error.ts
+++ src/Error.ts
@@ -4,14 +4,15 @@ export class YamlQLError extends Error {
 
   constructor(opts: string | Error, attach: any = {}) {
     if (opts instanceof Error) {
-      const { name, message, stack } = opts;
+      const { name, message, stack, attach } = opts as YamlQLError;
       super(message);
       this.name = name;
       this.stack = stack;
+      this.attach = attach;
     } else {
       super(opts);
     }
-    this.attach = attach;
+    this.attach = Object.assign({}, this.attach, attach);
   }
 
   toJSON() {
