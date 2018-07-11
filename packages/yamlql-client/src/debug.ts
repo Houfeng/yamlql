fe44cd21faba3e402b4094e6966e3b599548c30c
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2018-07-12 07:11:19 +0800

    更新示例

--- packages/yamlql-client/src/debug.ts
+++ packages/yamlql-client/src/debug.ts
@@ -1,17 +1,17 @@
 import Client from './index';
 
 const client = new Client({
-  endpoint: 'http://local.aliyun.com:7001/yamlql',
-  metadata: { QKEY_NAME: 'demo', QKEY_VALUE: 'e00bb831-d1a2-1165-dfea-7ddf9b091e6d' },
+  endpoint: 'https://query.aliyun.com/yamlql/',
+  metadata: { QKEY_NAME: 'demo', QKEY_VALUE: 'ea0cac65-6a6c-5e3f-7b5c-62e09bd7587b' },
 });
-   
-(async () => {         
  
+(async () => {
+
   try {
     const rs = await client.exec('./demo');
     console.log('成功', rs);
-  } catch (err) {   
-    console.error('失败', err);       
-  }         
- 
+  } catch (err) { 
+    console.error('失败', err);
+  }
+
 })();                     
